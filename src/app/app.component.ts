b304833f640651d702a0ddb054e98bed08837deb
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-09 20:05:01 -0600

    set up html on our website showing the count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { HospitalTotalService } from './hospital/hospital-total.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'CountingIndividuals';
+  title = 'Counting Individuals Project';
+  totalCount = 0;
+  // we put our value inside the constructor so it's ready when the page is rendered
+  constructor(private totalCounterService: HospitalTotalService) {
+    this.totalCount = totalCounterService.countTotal();
+  }
 }
