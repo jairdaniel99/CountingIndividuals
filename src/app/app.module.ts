42790112b6eaee1e1d765918cb34bd0f205c1b22
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-09 20:01:02 -0600

    created the hospital component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HospitalComponent } from './hospital/hospital.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hospital/hospital.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { HospitalTotalService } from './hospital-total.service';
+
+@Component({
+  selector: 'app-hospital',
+  templateUrl: './hospital.component.html',
+  styleUrl: './hospital.component.css',
+})
+export class HospitalComponent {}
